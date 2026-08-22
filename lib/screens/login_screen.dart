297a83d9d05a274d53c1d52e6205228a0112e7bb
Rostyslav Makhanko <rostik270900@gmail.com>
2026-08-22 19:59:44 +0200

    Add email field error message

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -46,7 +46,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 setState(() {
                   password = value;
                 });
-              }, showError: false),
+              }, showError: true),
             ],
           ),
         ),
@@ -67,68 +67,93 @@ class EmailField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: showError ? Colors.red : Colors.grey,
-          width: 1,
-        ),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
-      child: Row(
-        children: [
-          Container(
-            width: 34,
-            height: 34,
-            // decoration: BoxDecoration(
-            //   color: showError ? Colors.red.shade100 : Colors.grey.shade100,
-            //   borderRadius: BorderRadius.circular(10),
-            // ),
-            child: Icon(
-              Icons.mail_outline,
-              size: 18,
-              color: showError ? Colors.red.shade600 : Colors.grey.shade600,
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: showError ? Colors.red : Colors.grey,
+              width: 1,
             ),
+            borderRadius: BorderRadius.circular(10),
           ),
-          const SizedBox(width: 12),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'EMAIL',
-                  style: TextStyle(
-                    fontSize: 11,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey.shade600,
-                    letterSpacing: 0.5,
-                  ),
+          padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
+          child: Row(
+            children: [
+              Container(
+                width: 34,
+                height: 34,
+                // decoration: BoxDecoration(
+                //   color: showError ? Colors.red.shade100 : Colors.grey.shade100,
+                //   borderRadius: BorderRadius.circular(10),
+                // ),
+                child: Icon(
+                  Icons.mail,
+                  size: 32,
+                  color: showError ? Colors.red.shade600 : Colors.grey.shade600,
                 ),
-                TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'you@company.com',
-                    hintStyle: TextStyle(
-                      color: Colors.grey.shade500,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: -1,
+              ),
+
+              const SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'EMAIL',
+                      style: TextStyle(
+                        fontSize: 11,
+                        fontWeight: FontWeight.bold,
+                        color: showError ? Colors.red.shade600 : Colors.grey.shade600,
+                        letterSpacing: 0.5,
+                      ),
                     ),
-                    isDense: true,
-                    contentPadding: EdgeInsets.only(top: 2, bottom: 2),
-                  ),
-                  onChanged: onChanged,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  )
+                    TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'you@company.com',
+                        hintStyle: TextStyle(
+                          color: Colors.grey.shade500,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: -1,
+                        ),
+                        isDense: true,
+                        contentPadding: EdgeInsets.only(top: 2, bottom: 2),
+                      ),
+                      onChanged: onChanged,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: -1,
+                      )
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
+          ),
+        ),
+
+        if (showError) ...[
+          const SizedBox(height: 8),
+          Row(
+            children: [
+              Icon(Icons.error, color: Colors.red.shade600, size: 16),
+              const SizedBox(width: 8),
+              Text(
+                'Enter a valid email address',
+                style: TextStyle(
+                  color: Colors.red.shade600,
+                  fontSize: 12,
+                  letterSpacing: -0.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
           ),
         ],
-      ),
+      ],
     );
   }
 }
