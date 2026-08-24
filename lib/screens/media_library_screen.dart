813dcde6761984c69fa01ad61ccdc151cc3276cc
Rostyslav Makhanko <rostik270900@gmail.com>
2026-08-24 11:20:56 +0200

    Add folders list with 5 items

--- lib/screens/media_library_screen.dart
+++ lib/screens/media_library_screen.dart
@@ -8,25 +8,58 @@ class MediaLibraryScreen extends StatefulWidget {
 }
 
 class _MediaLibraryScreenState extends State<MediaLibraryScreen> {
-  List<String> folders = [];
+  List<String> folders = ['Clouds', 'Cars', 'Urban', 'Mountains', 'Ocean'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(title: const Text(''), backgroundColor: Colors.white),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 18),
+          padding: const EdgeInsets.only(top: 12),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 90),
-              const Text(
-                'Media Library',
-                style: TextStyle(
-                  fontSize: 34,
-                  fontWeight: FontWeight.w800,
-                  letterSpacing: -1,
+              Padding(
+                padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+                child: 
+                  Column(
+                    children: [
+                      const SizedBox(height: 12),
+                      const Text(
+                        'Media Library',
+                        style: TextStyle(
+                          fontSize: 34,
+                          fontWeight: FontWeight.w800,
+                          letterSpacing: -1,
+                        ),
+                      ),
+                    ],
+                  ),
+              ),
+
+              const Divider(height: 1),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: folders.length,
+                  itemBuilder: (context, index) {
+                    final folder = folders[index];
+
+                    return Column(
+                      children: [
+                        ListTile(
+                          leading: const Icon(Icons.folder, color: Colors.blue),
+                          title: Text(folder),
+                          trailing: const Icon(Icons.chevron_right),
+                          onTap: () {
+                            print('Tapped $folder');
+                          },
+                        ),
+                        const Divider(height: 1),
+                      ],
+                    );
+                  },
                 ),
               ),
             ],
