27335c482dbe8b19c0d889bfc57904367ac6e763
Rostyslav Makhanko <rostik270900@gmail.com>
2026-08-24 23:01:51 +0200

    Remove unused code

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -295,7 +295,9 @@ class PasswordField extends StatelessWidget {
               // const SizedBox(width: 8),
               IconButton(
                 icon: Icon(
-                  obscure ? Icons.visibility_outlined : Icons.visibility_off_outlined,
+                  obscure
+                      ? Icons.visibility_outlined
+                      : Icons.visibility_off_outlined,
                   color: Colors.grey.shade500,
                 ),
                 onPressed: toggelObscure,
@@ -387,63 +389,4 @@ class LoginButton extends StatelessWidget {
       ),
     );
   }
-}
-
-// Widget _buildEmailField(bool showError) {
-//   return Container(
-//     decoration: BoxDecoration(
-//       border: Border.all(
-//         color: showError ? Colors.red : Colors.grey,
-//         width: 1,
-//       ),
-//       borderRadius: BorderRadius.circular(10),
-//     ),
-//     child: Row(
-//       children: [
-//         Container(
-//           width: 34,
-//           height: 34,
-//           // decoration: BoxDecoration(
-//           //   color: showError ? Colors.red.shade100 : Colors.grey.shade100,
-//           //   borderRadius: BorderRadius.circular(10),
-//           // ),
-//           child: Icon(
-//             Icons.mail_outline,
-//             size: 18,
-//             color: showError ? Colors.red.shade600 : Colors.grey.shade600,
-//           ),
-//         ),
-//         const SizedBox(width: 12),
-//         Expanded(
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             children: [
-//               Text(
-//                 'EMAIL',
-//                 style: TextStyle(
-//                   fontSize: 11,
-//                   fontWeight: FontWeight.w600,
-//                   color: Colors.grey.shade600,
-//                   letterSpacing: 0.5,
-//                 ),
-//               ),
-//               TextField(
-//                 keyboardType: TextInputType.emailAddress,
-//                 decoration: const InputDecoration(
-//                   border: InputBorder.none,
-//                 ),
-//                 onChanged: (value) {
-//                   setState()
-//                 },
-//               ),
-//             ],
-//           ),
-//         ),
-//       ],
-//     ),
-//   );
-// }
-
-Widget _buildPasswordField() {
-  return Container();
 }
